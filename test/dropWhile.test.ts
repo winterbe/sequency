e7d7d6e6ee350c8b419a508b38a481b2f3a4e599
Benjamin Winterberg <winterbe@gmail.com>
2017-08-31 21:33:27 +0200

    Test

--- test/dropWhile.test.ts
+++ test/dropWhile.test.ts
@@ -2,13 +2,13 @@ import {sequenceOf} from "../src/Sequence";
 
 describe("dropWhile", () => {
     it("should drop elements until predicate evaluates to false", () => {
-        const result = sequenceOf(1, 2, 3, 4, 5)
+        const result = sequenceOf(1, 2, 3, 2, 1)
             .dropWhile(it => it < 3)
             .toArray();
         expect(result.length).toBe(3);
         expect(result[0]).toBe(3);
-        expect(result[1]).toBe(4);
-        expect(result[2]).toBe(5);
+        expect(result[1]).toBe(2);
+        expect(result[2]).toBe(1);
     });
 
     it("should drop no elements", () => {
