555ba1205925786bff89b0e8135c72467e6ad64f
Benjamin Winterberg <winterbe@gmail.com>
2018-07-30 08:49:25 +0200

    #12 Improve docs

--- src/merge.ts
+++ src/merge.ts
@@ -6,7 +6,7 @@ export class Merge {
      * Merges the elements of both sequences into a new sequence. Each element of this sequence is eventually replaced with
      * an element of the other sequence by comparing results of the given `selector` function. If no value is found in the other
      * sequence the element is retained. New elements of the other sequence are appended to the end of the new sequence or
-     * prepended to the start of the new sequence, if `prependNewValues` is set to `true`.
+     * prepended to the start of the new sequence, if `prependNewValues` is set to `true`. This operation is not lazy evaluated.
      *
      * @param {Sequence<T>} other
      * @param {(value: T) => S} selector
